3206c8d7b5f8400b5d63a8fa546514e19305c1fe
gKim1GW <g.kim1@gwu.edu>
2025-09-25 21:45:05 -0400

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,31 @@
+import time, math
+
+def run_2nd(n, a, b):   #project option 2
+    sum_ = 0
+    j = 2
+    while j < n:
+        k = 2                
+        while k < n:
+            idx = int(k)  # Convert k to int because list a, b indices must be integers
+            sum_ += a[idx] * b[idx]
+            k = k * math.sqrt(k)  
+        j += j // 2             
+
+# To reduce experimental noise, ran 999 trials for each n and used the median
+def get_time(n, reps=999):
+    times = []
+    a = list(range(n)) # Size n is sufficient (k < n)
+    b = list(range(n))
+    for _ in range(reps):
+        t1 = time.perf_counter_ns()
+        run_2nd(n, a, b)
+        t2 = time.perf_counter_ns()
+        times.append(t2 - t1)
+    times.sort()
+    return times[len(times)//2]   
+
+# n values to test
+data_list = [10, 100, 1000, 10000, 100000]
+
+for n in data_list:
+    print(f"{n},{get_time(n)}")
